f71b821f623a92aab90717ef306b8eb082e3030f
gaurav bisht <Gaurav.Bisht1@T-Mobile.com>
2017-09-12 16:24:36 +0530

    Primeng css override commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarModule } from 'primeng/primeng';
+import { EditorModule } from 'primeng/primeng';
+import { DropdownModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { HomeComponent } from './home/home.component';
+import { EditorComponent } from './editor/editor.component';
+import { CustomComponent } from './custom/custom.component';
 
 const routes: Routes = [
   { path: 'hero', component: HeroComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'editor', component: EditorComponent},
+  { path: 'custom', component: CustomComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    HomeComponent
+    HomeComponent,
+    EditorComponent,
+    CustomComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    EditorModule,
+    DropdownModule,
     CalendarModule,
     RouterModule.forRoot(routes)
   ],
